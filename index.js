1db6d91641def208b12dd00771a4fbdeb1941b18
NoibaX <abionjustinekarl1@gmail.com>
2025-04-08 12:07:25 +0800

    Simon Game
    Add Repository to GitHub

---
+++ index.js
@@ -0,0 +1,93 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+    else {
+    var userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+
+
+    playsound(userChosenColor);
+    animatePress(userChosenColor);      
+
+    checkAnswer(userClickedPattern.length - 1);
+    }
+
+});
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+        console.log("Success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }
+
+    else {
+        console.log("Wrong");
+        playsound("wrong");
+        $("body").addClass("game-over");
+
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game over, Press Any Key To Restart");
+
+        startOver();
+    }
+
+}
+
+function nextSequence() {
+
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playsound(randomChosenColor);
+
+    level++;
+
+    $("#level-title").text("Level " + level);
+
+    userClickedPattern = [];
+    
+}
+
+function playsound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 110);
+
+}
